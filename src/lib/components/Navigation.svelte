6969c87b75ffb0ff2b65b55d8ddf2672b44e3666
kevinclelland <kevinclelland@gmail.com>
2023-07-09 14:31:00 +0200

    Removed undeveloped freatures for now

--- src/lib/components/Navigation.svelte
+++ src/lib/components/Navigation.svelte
@@ -13,10 +13,10 @@
 		<li><a href="/athleteprofiles" on:click={drawerClose}>Athlete Profiles</a></li>
 		<li><a href="/tools" on:click={drawerClose}>Tools</a></li>
 		<li><a href="/racereports" on:click={drawerClose}>Race Report Almanac</a></li>
-		<li><a href="/forum" on:click={drawerClose}>Forum</a></li>
-		<li><a href="/blog" on:click={drawerClose}>Blog</a></li>
 		<li><a href="/resources" on:click={drawerClose}>Resources</a></li>
-		<li><a href="/almanac" on:click={drawerClose}>Almanac</a></li>
 		<li><a href="/contact" on:click={drawerClose}>About and Contact</a></li>
+		<!--<li><a href="/almanac" on:click={drawerClose}>Almanac</a></li>
+		<li><a href="/forum" on:click={drawerClose}>Forum</a></li>
+		<li><a href="/blog" on:click={drawerClose}>Blog</a></li>-->
 	</ul>
 </nav>
