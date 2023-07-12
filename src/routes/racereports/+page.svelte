c631a46ca49c5ac785f84c33cdc4b0f97f17ea51
kevinclelland <kevinclelland@gmail.com>
2023-07-12 05:21:02 +0200

    Updated almanac landing page

--- src/routes/racereports/+page.svelte
+++ src/routes/racereports/+page.svelte
@@ -12,34 +12,72 @@
 
 
 
-<!-- Posts -->
-<section>
-	<div class="text-center text-6xl p-4">Race Report Almanac</div>
+<header class="w-full">
+	<nav class="flex flex-col md:flex-row gap-3 items-center justify-around py-5 px-4">
+		<div class="md:w-2/12 text-center md:text-left">
+			<p class="font-serif text-xl font-bold">By Aero Oats.</p>
+			<p class="font-serif text-sm">
+				Bi-weekly triathlon newsletter for you to chew through while you munch your morning oats.
+			</p>
+		</div>
+		<div>
+			<h1 class="font-serif text-4xl font-bold text-center">THE RACE REPORT ALMANAC</h1>
+			<h1 class="text-center text-2xl">Global Distribution</h1>
+		</div>
+		<div
+			class="flex md:w-2/12 w-full flex-col items-center justify-center border border-black text-xl font-bold"
+		>
+			<h1>COMMUNITY SUPPLIED</h1>
+			<h1>AND TESTED!</h1>
+		</div>
+	</nav>
+</header>
 
-	<div class="mx-auto text-token grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
-		<!-- Detailed -->
-		{#each data.posts as post}
-			<div class="col-span-1 card card-hover max-w-2xl overflow-hidden">
-				<header class="flex justify-around">
-					<img src={post.photo} class="bg-black/50 max-h-80" alt="Post" />
-				</header>
-				<div class="p-4 space-y-4">
-					<h3 class="h3"><a href={post.slug} class="title">{post.title}</a></h3>
-
-					<article>
-						<p class="description">{post.description}</p>
-					</article>
-				</div>
-
-				<hr class="opacity-50" />
-				<footer class="p-4 flex justify-start items-center space-x-4">
-					<Avatar src={post.avatar} width="w-8" />
-					<div class="flex-auto flex justify-between items-center">
-						<h6 class="font-bold">{post.user}</h6>
-						<small class="date">On {formatDate(post.date)}</small>
-					</div>
-				</footer>
-			</div>
-		{/each}
+<section
+	class="mx-auto flex items-center justify-between border-t border-b border-black py-3 px-5 font-thin"
+>
+	<div>
+		
+		<span>Athletes: 1</span>
+		
+	</div>
+	<span>Updated 12 July 2023</span>
+	<div class="flex items-center">
+		
+		<p class="px-2">Races Featured: 1</p>
 	</div>
 </section>
+
+
+<!-- Post -->
+<div class="p-10">
+	
+		<div class="mx-auto text-token grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
+			<!-- Detailed -->
+			{#each data.posts as post}
+				<div class="col-span-1 card card-hover max-w-2xl overflow-hidden">
+					<header class="flex justify-around">
+						<img src={post.photo} class="bg-black/50 max-h-80" alt="Post" />
+					</header>
+					<div class="p-4 space-y-4">
+						<h3 class="h3"><a href={post.slug} class="title">{post.title}</a></h3>
+	
+						<article>
+							<p class="description">{post.description}</p>
+						</article>
+					</div>
+	
+					<hr class="opacity-50" />
+					<footer class="p-4 flex justify-start items-center space-x-4">
+						<Avatar src={post.avatar} width="w-8" />
+						<div class="flex-auto flex justify-between items-center">
+							<h6 class="font-bold">{post.user}</h6>
+							<small class="date">On {formatDate(post.date)}</small>
+						</div>
+					</footer>
+				</div>
+			{/each}
+		</div>
+	
+</div>
+
