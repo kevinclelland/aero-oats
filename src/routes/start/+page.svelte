27a35cd3a19f019f567641ecb919d9bb2f3593c1
kevinclelland <kevinclelland@gmail.com>
2023-08-12 05:36:55 +0200

    More content

--- src/routes/start/+page.svelte
+++ src/routes/start/+page.svelte
@@ -176,7 +176,7 @@
 			
 			<Step on:complete={onCompleteHandler}>
 				<svelte:fragment slot="header">Watch This</svelte:fragment>
-				Coming soon
+				<iframe class="w-full aspect-video" src="https://www.youtube.com/embed/ovA7G6IM_is" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
 			</Step>
 			<!-- ... -->
 		</Stepper>
