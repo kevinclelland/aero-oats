59186222f686833372ff79a19139cc9e530f1cba
kevinclelland <kevinclelland@gmail.com>
2023-08-20 05:48:55 +0200

    Added race countdown

--- src/lib/components/Upcoming.svelte
+++ src/lib/components/Upcoming.svelte
@@ -1,53 +1,298 @@
-<script>
-	const listData = [
-		{
-			link: 'https://protriathletes.org/events/pto-tour/european-open/pro/',
-			name: 'PTO European Open',
-			label: '6 May - Ibiza, Spain'
-		},
-		{
-			link: 'https://www.challenge-roth.com/en/',
-			name: 'Challenge Roth',
-			label: '25 June - Roth, Germany'
-		},
-		{
-			link: 'https://protriathletes.org/events/pto-tour/us-open/pro/',
-			name: 'PTO US Open',
-			label: '5 August - Milwaukee, USA'
-		},
-		{
-			link: 'https://protriathletes.org/events/pto-tour/asian-open/pro/',
-			name: 'PTO Asian Open',
-			label: '20 August - Singapore'
-		},
-		{
-			link: 'https://www.ironman.com/im703-world-championship-2023',
-			name: 'Ironman 70.3 WC',
-			label: '26/27 August - Lahti,Finland'
-		},
-		{
-			link: 'https://www.ironman.com/im-world-championship-nice',
-			name: 'Ironman WC Men',
-			label: '10 September - Nice, France'
-		},
-		{
-			link: 'https://www.ironman.com/im-world-championship-2023',
-			name: 'Ironman WC Women',
-			label: '14 October - Kona, Hawaii'
-		}
-	];
+<script lang="ts">
+import { onMount } from 'svelte';
+
+var endOfEurope = new Date("May 6, 2023, 09:00:00").getTime();
+var endOfRoth = new Date("June 25, 2023, 09:00:00").getTime();
+var endOfUS = new Date("Aug 5, 2023, 09:00:00").getTime();
+var endOfAsia = new Date("Aug 20, 2023, 09:00:00").getTime();
+var endOf703W = new Date("Aug 26, 2023, 07:30:00").getTime();
+var endOf703M = new Date("Aug 27, 2023, 07:30:00").getTime();
+var endOfWCMEN = new Date("Sep 10, 2023, 09:00:00").getTime();
+var endOfWCWOMEN = new Date("Oct 14, 2023, 09:00:00").getTime();
+
+var days :number;
+var hours :number;
+var minutes :number; 
+var seconds :number ;
+var outputEurope :any;
+var outputRoth :any;
+var outputUS :any;
+var outputAsia :any;
+var output703W :any;
+var output703M :any;
+var outputWCMEN :any;
+var outputWCWOMEN :any;
+let now: number = new Date().getTime();
+let distance: number;
+let distanceRoth: number;
+let distanceUS: number;
+let distanceAsia: number;
+let distance703: number;
+let distanceWCMEN: number;
+let distanceWCWOMEN: number;
+
+onMount(() => {
+const interval = setInterval(() => {
+	  now = new Date().getTime();
+}, 1000);
+
+return () => { clearInterval(interval);
+};
+});
+  
+function time(now: number) {
+
+  distance = endOfEurope - now;
+
+  days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  seconds = Math.floor((distance % (1000*60)) / 1000);
+
+  return outputEurope = days + " days " + hours + " h " + minutes + " m " + seconds + " s";
+ 
+  
+}
+
+function timeRoth(now: number) {
+
+distanceRoth = endOfRoth - now;
+
+var daysRoth = Math.floor(distanceRoth / (1000 * 60 * 60 * 24));
+var hoursRoth = Math.floor((distanceRoth % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+var minutesRoth = Math.floor((distanceRoth % (1000 * 60 * 60)) / (1000 * 60));
+var secondsRoth = Math.floor((distanceRoth % (1000*60)) / 1000);
+
+return outputRoth = daysRoth + " days " + hoursRoth + " h " + minutesRoth + " m " + secondsRoth + " s";
+
+}
+
+
+function timeUS(now: number) {
+
+distanceUS = endOfUS - now;
+
+var daysUS = Math.floor(distanceUS / (1000 * 60 * 60 * 24));
+var hoursUS = Math.floor((distanceUS % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+var minutesUS = Math.floor((distanceUS % (1000 * 60 * 60)) / (1000 * 60));
+var secondsUS = Math.floor((distanceUS % (1000*60)) / 1000);
+
+return outputUS = daysUS + " days " + hoursUS + " h " + minutesUS + " m " + secondsUS + " s";
+
+}
+
+
+function timeAsia(now: number) {
+
+distanceAsia = endOfAsia - now;
+
+var daysAsia = Math.floor(distanceAsia / (1000 * 60 * 60 * 24));
+var hoursAsia = Math.floor((distanceAsia % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+var minutesAsia = Math.floor((distanceAsia % (1000 * 60 * 60)) / (1000 * 60));
+var secondsAsia = Math.floor((distanceAsia % (1000*60)) / 1000);
+
+return outputAsia = daysAsia + " days " + hoursAsia + " h " + minutesAsia + " m " + secondsAsia + " s";
+
+}
+
+function time703W(now: number) {
+
+var distance703W = endOf703W - now;
+
+var days703W = Math.floor(distance703W / (1000 * 60 * 60 * 24));
+var hours703W = Math.floor((distance703W % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+var minutes703W = Math.floor((distance703W % (1000 * 60 * 60)) / (1000 * 60));
+var seconds703W = Math.floor((distance703W % (1000*60)) / 1000);
+
+
+return output703W = days703W + " days " + hours703W + " h " + minutes703W + " m " + seconds703W + " s";
+
+}
+
+
+function time703M(now: number) {
+
+var distance703M = endOf703M - now;
+
+var days703M = Math.floor(distance703M / (1000 * 60 * 60 * 24));
+var hours703M = Math.floor((distance703M % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+var minutes703M = Math.floor((distance703M % (1000 * 60 * 60)) / (1000 * 60));
+var seconds703M = Math.floor((distance703M % (1000*60)) / 1000);
+
+return output703M = days703M + " days " + hours703M + " h " + minutes703M + " m " + seconds703M + " s";
+
+}
+
+
+
+function timeWCMEN(now: number) {
+
+distanceWCMEN = endOfWCMEN - now;
+
+var daysWCMEN = Math.floor(distanceWCMEN / (1000 * 60 * 60 * 24));
+var hoursWCMEN = Math.floor((distanceWCMEN % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+var minutesWCMEN = Math.floor((distanceWCMEN % (1000 * 60 * 60)) / (1000 * 60));
+var secondsWCMEN = Math.floor((distanceWCMEN % (1000*60)) / 1000);
+
+return outputWCMEN = daysWCMEN + " days " + hoursWCMEN + " h " + minutesWCMEN + " m " + secondsWCMEN + " s";
+
+}
+
+function timeWCWOMEN(now: number) {
+
+distanceWCWOMEN = endOfWCWOMEN - now;
+
+var daysWCWOMEN = Math.floor(distanceWCWOMEN / (1000 * 60 * 60 * 24));
+var hoursWCWOMEN = Math.floor((distanceWCWOMEN % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+var minutesWCWOMEN = Math.floor((distanceWCWOMEN % (1000 * 60 * 60)) / (1000 * 60));
+var secondsWCWOMEN = Math.floor((distanceWCWOMEN % (1000*60)) / 1000);
+
+return outputWCWOMEN = daysWCWOMEN + " days " + hoursWCWOMEN + " h " + minutesWCWOMEN + " m " + secondsWCWOMEN + " s";
+
+}
+
+	
 </script>
 
+
 <dl class="list-dl">
-	{#each listData as v}
-		<div>
-			<span class="badge-icon p-2 variant-soft-secondary text-2xl">🗓️</span>
-			<a href="/{v.link}"
-				><span class="flex-auto">
-					<dt class="font-bold">{v.name}</dt>
-					<dd class="text-sm opacity-50 text-ellipsis">{v.label}</dd>
-				</span></a
-			>
-		</div>
-	{/each}
+	<div>
+		<span class="badge-icon p-2 variant-soft-secondary text-2xl">🗓️</span>
+		<a href="https://protriathletes.org/events/pto-tour/european-open/pro/"
+			><span class="flex-auto">
+				<dt class="font-bold">PTO European Open</dt>
+				<dd class="text-sm opacity-50 text-ellipsis">6 May - Ibiza, Spain</dd>
+				<dd class="text-sm opacity-50 text-ellipsis">
+				
+					{#if distance < 1}
+					<p>Complete</p>
+					{:else}
+					{time(now)} 
+					{/if}
+
+				</dd>
+			</span></a
+		>
+	</div>
+
+	<div>
+		<span class="badge-icon p-2 variant-soft-secondary text-2xl">🗓️</span>
+		<a href="https://www.challenge-roth.com/en/"
+			><span class="flex-auto">
+				<dt class="font-bold">Challenge Roth</dt>
+				<dd class="text-sm opacity-50 text-ellipsis">25 June - Roth, Germany</dd>
+				<dd class="text-sm opacity-50 text-ellipsis">
+				
+					{#if distanceRoth < 1}
+					<p>Complete</p>
+					{:else}
+					{timeRoth(now)} 
+					{/if}
+				
+				</dd>
+			</span></a
+		>
+	</div>
+
+	<div>
+		<span class="badge-icon p-2 variant-soft-secondary text-2xl">🗓️</span>
+		<a href="https://protriathletes.org/events/pto-tour/us-open/pro/"
+			><span class="flex-auto">
+				<dt class="font-bold">PTO US Open</dt>
+				<dd class="text-sm opacity-50 text-ellipsis">5 August - Milwaukee, USA</dd>
+				<dd class="text-sm opacity-50 text-ellipsis">
+				
+					{#if distanceUS < 1}
+					<p>Complete</p>
+					{:else}
+					{timeUS(now)} 
+					{/if}
+					
+				</dd>
+			</span></a
+		>
+	</div>
+
+	<div>
+		<span class="badge-icon p-2 variant-soft-secondary text-2xl">🗓️</span>
+		<a href="https://protriathletes.org/events/pto-tour/asian-open/pro/"
+			><span class="flex-auto">
+				<dt class="font-bold">PTO Asian Open</dt>
+				<dd class="text-sm opacity-50 text-ellipsis">20 August - Singapore</dd>
+				<dd class="text-sm opacity-50 text-ellipsis">
+
+					{#if distanceAsia < 1}
+					<p>Complete</p>
+					{:else}
+					L: Complete <br>
+					M: {timeAsia(now)} 
+					{/if}
+
+
+				</dd>
+			</span></a
+		>
+	</div>
+
+
+	<div>
+		<span class="badge-icon p-2 variant-soft-secondary text-2xl">🗓️</span>
+		<a href="https://www.ironman.com/im703-world-championship-2023"
+			><span class="flex-auto">
+				<dt class="font-bold">Ironman 70.3 WC</dt>
+				<dd class="text-sm opacity-50 text-ellipsis">26/27 August - Lahti,Finland</dd>
+				<dd class="text-sm opacity-50 text-ellipsis">
+
+					{#if distance703 < 1}
+					<p>Complete</p>
+					{:else}
+					L: {time703W(now)}<br>
+					M:{time703M(now)}
+					{/if}
+
+				</dd>
+			</span></a
+		>
+	</div>
+
+	<div>
+		<span class="badge-icon p-2 variant-soft-secondary text-2xl">🗓️</span>
+		<a href="https://www.ironman.com/im-world-championship-nice"
+			><span class="flex-auto">
+				<dt class="font-bold">Ironman WC Men</dt>
+				<dd class="text-sm opacity-50 text-ellipsis">10 September - Nice, France</dd>
+				<dd class="text-sm opacity-50 text-ellipsis">
+
+					{#if distanceWCMEN < 1}
+					<p>Complete</p>
+					{:else}
+					{timeWCMEN(now)} 
+					{/if}
+
+				</dd>
+			</span></a
+		>
+	</div>
+
+	<div>
+		<span class="badge-icon p-2 variant-soft-secondary text-2xl">🗓️</span>
+		<a href="https://www.ironman.com/im-world-championship-2023"
+			><span class="flex-auto">
+				<dt class="font-bold">Ironman WC Women</dt>
+				<dd class="text-sm opacity-50 text-ellipsis">14 October - Kona, Hawaii</dd>
+				<dd class="text-sm opacity-50 text-ellipsis">
+
+					{#if distanceWCWOMEN < 1}
+					<p>Complete</p>
+					{:else}
+					{timeWCWOMEN(now)} 
+					{/if}
+
+				</dd>
+			</span></a
+		>
+	</div>
+
 </dl>
+
+
